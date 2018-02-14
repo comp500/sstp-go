ae9df321311501e4efbec8b5a5728c2d92c6c982
comp500 <comp500@users.noreply.github.com>
2018-02-14 20:06:36 +0000

    No escape before dump

--- unpack.go
+++ unpack.go
@@ -53,7 +53,7 @@ func handleDataPacket(dataHeader sstpDataHeader, conn net.Conn, pppdInstance *pp
 		log.Fatal("pppd instance not started")
 	} else {
 		n, err := pppdInstance.stdin.Write(pppEscape(dataHeader.Data))
-		fmt.Print(hex.Dump(pppEscape(dataHeader.Data)))
+		fmt.Print(hex.Dump(dataHeader.Data))
 		handleErr(err)
 		log.Printf("%v bytes written to pppd", n)
 	}
