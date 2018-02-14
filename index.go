24874e077490f280729d13aecd5a140c5b1e379e
comp500 <comp500@users.noreply.github.com>
2018-02-14 12:22:41 +0000

    Add loads of pppd stuff

--- index.go
+++ index.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"net"
+	"os/exec"
 )
 
 func handleErr(err error) {
@@ -107,7 +108,7 @@ type sstpDataHeader struct {
 	Data []byte
 }
 
-func handlePacket(input []byte, conn net.Conn) {
+func handlePacket(input []byte, conn net.Conn, pppdInstance *exec.Cmd) {
 	header := sstpHeader{}
 
 	header.MajorVersion = input[0] >> 4
@@ -135,7 +136,7 @@ func handlePacket(input []byte, conn net.Conn) {
 		}
 		controlHeader.Attributes = attributes
 
-		handleControlPacket(controlHeader, conn)
+		handleControlPacket(controlHeader, conn, pppdInstance)
 		return
 	}
 
@@ -143,14 +144,22 @@ func handlePacket(input []byte, conn net.Conn) {
 	dataHeader.sstpHeader = header
 	dataHeader.Data = input[4:(len(input) - 4)]
 
-	handleDataPacket(dataHeader, conn)
+	handleDataPacket(dataHeader, conn, pppdInstance)
 }
 
-func handleDataPacket(dataHeader sstpDataHeader, conn net.Conn) {
+func handleDataPacket(dataHeader sstpDataHeader, conn net.Conn, pppdInstance *exec.Cmd) {
 	log.Printf("read: %v\n", dataHeader)
+	if pppdInstance == nil {
+		log.Fatal("pppd instance not started")
+	} else {
+		pppIn, _ := pppdInstance.StdinPipe()
+		n, err := pppIn.Write(dataHeader.Data)
+		handleErr(err)
+		log.Printf("%v bytes written to pppd", n)
+	}
 }
 
-func handleControlPacket(controlHeader sstpControlHeader, conn net.Conn) {
+func handleControlPacket(controlHeader sstpControlHeader, conn net.Conn, pppdInstance *exec.Cmd) {
 	log.Printf("read: %v\n", controlHeader)
 
 	if controlHeader.MessageType == MessageTypeCallConnectRequest {
@@ -158,6 +167,9 @@ func handleControlPacket(controlHeader sstpControlHeader, conn net.Conn) {
 		// TODO: implement Nak?
 		// -> if protocols specified by req not supported
 		// however there is only PPP currently, so not a problem
+		pppdInstance = createPPPD()
+		log.Print("pppd instance created")
+		addPPPDResponder(pppdInstance, conn)
 	} else if controlHeader.MessageType == MessageTypeCallDisconnect {
 		sendDisconnectAckPacket(conn)
 	} else if controlHeader.MessageType == MessageTypeEchoRequest {
@@ -233,6 +245,81 @@ func sendEchoResponsePacket(conn net.Conn) {
 	outputBytes := make([]byte, 8)
 	packControlHeader(controlHeader, outputBytes)
 	conn.Write(outputBytes)
+}
+
+func packDataHeader(header sstpDataHeader, outputBytes []byte) {
+	packHeader(header.sstpHeader, outputBytes[0:4])
+	copy(header.Data, outputBytes[4:(len(header.Data)-4)])
+}
+
+func sendDataPacket(inputBytes []byte, conn net.Conn) {
+	length := 8 + len(inputBytes)
+	header := sstpHeader{1, 0, false, uint16(length)}
+	dataHeader := sstpDataHeader{header, inputBytes}
+
+	log.Printf("write: %v\n", dataHeader)
+	packetBytes := make([]byte, length)
+	packDataHeader(dataHeader, packetBytes)
+	conn.Write(packetBytes)
+}
+
+func createPPPD() *exec.Cmd {
+	pppdCmd := exec.Command("pppd")
+	err := pppdCmd.Start()
+	handleErr(err)
+	return pppdCmd
+}
+
+func addPPPDResponder(pppdInstance *exec.Cmd, conn net.Conn) {
+	go func(pppdInstance *exec.Cmd, conn net.Conn) {
+		defer pppdInstance.Process.Kill()
+
+		ch := make(chan []byte)
+		eCh := make(chan error)
+		pppdOut, _ := pppdInstance.StdoutPipe()
+
+		// Start a goroutine to read from our net connection
+		go func(ch chan []byte, eCh chan error) {
+			for {
+				// try to read the data
+				data := make([]byte, 512)
+				n, err := pppdOut.Read(data)
+				fmt.Printf("pppd: %v bytes read", n)
+
+				if err != nil {
+					// send an error if it's encountered
+					eCh <- err
+					return
+				}
+				// send data if we read some.
+				ch <- data
+			}
+		}(ch, eCh)
+
+		//ticker := time.Tick(time.Second)
+		// continuously read from the connection
+		for {
+			select {
+			case data := <-ch: // This case means we recieved data on the connection
+				// Do something with the data
+				//log.Printf("%s\n", hex.Dump(data))
+				sendDataPacket(data, conn)
+			case err := <-eCh: // This case means we got an error and the goroutine has finished
+				if err == io.EOF {
+					log.Print("pppd disconnected")
+				} else {
+					log.Fatalf("pppd: %s\n", err)
+					// handle our error then exit for loop
+					break
+					// This will timeout on the read.
+					//case <-ticker:
+					// do nothing? this is just so we can time out if we need to.
+					// you probably don't even need to have this here unless you want
+					// do something specifically on the timeout.
+				}
+			}
+		}
+	}(pppdInstance, conn)
 }
 
 func main() {
@@ -319,6 +406,7 @@ func main() {
 
 			ch := make(chan []byte)
 			eCh := make(chan error)
+			var pppdInstance *exec.Cmd // store null pointer to future pppd instance
 
 			// Start a goroutine to read from our net connection
 			go func(ch chan []byte, eCh chan error) {
@@ -343,7 +431,7 @@ func main() {
 				case data := <-ch: // This case means we recieved data on the connection
 					// Do something with the data
 					//log.Printf("%s\n", hex.Dump(data))
-					handlePacket(data, conn)
+					handlePacket(data, conn, pppdInstance)
 				case err := <-eCh: // This case means we got an error and the goroutine has finished
 					if err == io.EOF {
 						log.Print("Client disconnected")
