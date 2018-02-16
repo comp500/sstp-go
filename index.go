ff103d6895b60247b2749cabae57a7c665028b50
comp500 <comp500@users.noreply.github.com>
2018-02-16 19:00:12 +0000

    CPU profiling

--- index.go
+++ index.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
+	"runtime/pprof"
 )
 
 func handleErr(err error) {
@@ -19,7 +22,19 @@ type parseReturn struct {
 	Data      []byte
 }
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
 	l, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Fatal(err)
